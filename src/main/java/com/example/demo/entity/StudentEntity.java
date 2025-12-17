2e55f2ca95581f8c2a252fb41906ae04089e1bf4
bala-amypo <bala@amypo.in>
2025-12-17 06:43:59 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -3,7 +3,7 @@ package com.example.demo.entity;
 import jakarta.presistance.Entity;
 import jakarta.presistance.Id;
 import jakrata.presistance.GenerationType;
-import jakarta.p
+import jakarta.presistance.GeneratedValue;
 @Entity
 public class StudentEntity(){
 
@@ -16,7 +16,30 @@ private String email;
 private String password;
 private Date created;
 
+}
+public String getName(){
+    return name;
+}
+public String setName(String name){
+    this.name=name;
+}
 
+public Integer getId(){
+    return id;
+}
+public String setId(Integer id){
+    this.id=id;
+}
 
-
+public String getEmail(){
+    return email;
+}
+public String setEmail(String email){
+    this.email=email;
+}
+public String getPassword(){
+    return password;
+}
+public String setPassword(String password){
+    this.password=name;
 }
