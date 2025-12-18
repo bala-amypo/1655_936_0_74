f0e4f63dae12d653f223b89fae9f6e93833fc0e1
bala-amypo <bala@amypo.in>
2025-12-18 08:14:28 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Positive;
 @Entity
 public ValidationEntity{
 
@@ -18,12 +19,11 @@ private Long id;
 private String name;
 @Email(message="Email is not valid")
 private String email;
-@Max(6,message="should be less than 6")
+@Size(min=2,max=6,message="should be less than 6")
 @NotNull
 private String password;
-@Max(message="age should be less than 35")
-@Positive
-private Number age;
-
+@Max(35,message="age should be less than 35")
+@Positive(message="Age cannot be negative")
+private int age;
 
 }
